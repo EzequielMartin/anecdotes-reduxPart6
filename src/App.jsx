0712b056487e90628679bdac589285eb1ee93831
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-09 15:57:23 -0300

    Hago que la app funcione usando un server local en jsonserver

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import Anecdotes from './components/Anecdotes'
 import { Filter } from './components/Filter'
+import anecdotesService from "./services/anecdotes"
+import { useEffect } from 'react'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdotesService
+      .getAll().then(a => dispatch(setAnecdotes(a)))
+  }, [])
+
 
   return (
     <div>
